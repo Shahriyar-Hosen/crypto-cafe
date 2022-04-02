9ebcecf9d489bc03dab0404e438c0b2620505f34
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-02 15:39:43 +0600

    Crypto Cafe setup

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className=''>
+            <div>Crypto Cafe</div>
+            <div></div>
+        </div>
+    );
+};
+
+export default Header;
