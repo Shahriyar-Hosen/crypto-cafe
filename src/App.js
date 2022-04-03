a9869c37423291586cdcd040e51606df355bd48e
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-03 08:01:39 +0600

    not found page added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import UsAddress from "./components/Contact/UsAddress";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Homepage from "./components/Homepage/Homepage";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Route path="us-address" element={<UsAddress />}></Route>
         </Route>
         <Route path="/about" element={<About />}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
