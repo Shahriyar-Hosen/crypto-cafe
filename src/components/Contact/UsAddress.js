6cc0e8ae8554427cb84231ef4860a9e95bed7cd0
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-03 07:58:06 +0600

    address style added

--- src/components/Contact/UsAddress.js
+++ src/components/Contact/UsAddress.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 const UsAddress = () => {
   return (
-    <div className='border p-8 bg-green-100 w-full'>
-      <div className='flex justify-between items-center'>
+    <div className="border p-8 bg-green-100 w-full">
+      <div className="flex flex-col md:flex-row justify-between items-center">
         <div>
           <h1>Street: 1203 SN Street</h1>
           <h1>City: New York</h1>
@@ -11,11 +11,11 @@ const UsAddress = () => {
           <h1>Email: contact.usa@cc.com</h1>
         </div>
         <div>
-          <h1 className='text-2xl'>USA OFFICE</h1>
+          <h1 className="text-2xl">USA OFFICE</h1>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default UsAddress
+export default UsAddress;
